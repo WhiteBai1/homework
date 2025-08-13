4911dadb9bab6e2cddd8a78cb3807a3b93c5adcc
Clouder0 <clouder0@outlook.com>
2025-08-13 20:28:12 +0800

    chore: format code

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,50 +1,53 @@
 #include <csignal>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int n;
-void maxheapfiy(int *a,int i,int len);
+void maxheapfiy(int *a, int i, int len);
 void buildMaxHeap(int *a);
 void heapsort(int *a);
 
-int main(){
-    cin >> n;
-    int arr[n];
-    for(int i =0;i<n;i++) cin >> arr[i];
+int main() {
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
 
-   heapsort(arr);
+  heapsort(arr);
 
-
-    for(int i=n-1;i>=0;i--) cout << arr[i] <<" ";
-    return 0;
+  for (int i = n - 1; i >= 0; i--)
+    cout << arr[i] << " ";
+  return 0;
 }
 
-void buildMaxHeap(int *a){
-    for(int i = n/2;i>=0;i--){
-        maxheapfiy(a,i,n);
-    }
+void buildMaxHeap(int *a) {
+  for (int i = n / 2; i >= 0; i--) {
+    maxheapfiy(a, i, n);
+  }
 }
 
-void maxheapfiy(int *a,int i,int len){
-    int l = 2*i,r=l+1;
-    int largest =i;
-    if(l<=len-1 && a[largest]<a[l]) largest = l;
-    if(r<=len-1 && a[largest]<a[r]) largest = r;
-    if(largest!=i){
-        int tmp = a[i];
-        a[i] = a[largest];
-        a[largest] = tmp;
-        maxheapfiy(a, largest,len);
-    }
-    return;
+void maxheapfiy(int *a, int i, int len) {
+  int l = 2 * i, r = l + 1;
+  int largest = i;
+  if (l <= len - 1 && a[largest] < a[l])
+    largest = l;
+  if (r <= len - 1 && a[largest] < a[r])
+    largest = r;
+  if (largest != i) {
+    int tmp = a[i];
+    a[i] = a[largest];
+    a[largest] = tmp;
+    maxheapfiy(a, largest, len);
+  }
+  return;
 }
 
-void heapsort(int *a){
-    buildMaxHeap(a);
-    for(int i = n-1;i>=1;i--){
-        int tmp = a[i];
-        a[i] = a[0];
-        a[0] = tmp;
-        maxheapfiy(a,0,i);
-    }
+void heapsort(int *a) {
+  buildMaxHeap(a);
+  for (int i = n - 1; i >= 1; i--) {
+    int tmp = a[i];
+    a[i] = a[0];
+    a[0] = tmp;
+    maxheapfiy(a, 0, i);
+  }
 }
