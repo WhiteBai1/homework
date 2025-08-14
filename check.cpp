177deb3f091b83b53dcc9985c688c799dd46a757
WhiteBai1 <WhiteBai1@outlook.com>
2025-08-14 19:34:28 +0800

    判断顺序、逆序，并扩展数据量

--- check.cpp
+++ check.cpp
@@ -1,20 +1,43 @@
+#include <fstream>
 #include <iostream>
-#include <vector>
 
 int main() {
-  std::vector<int> arr;
+  std::ios::sync_with_stdio(false);
+  std::ifstream infile("result2.txt");
+
+  bool order = true;
+  bool reverse = true;
+
   int x;
-  std::cin >> x;
+  infile >> x;
 
   int y;
-  while (std::cin >> y) {
+  while (infile >> y) {
     if (x < y) {
-      std::cout << 'F' << std::endl;
-      return 0;
+      // std::cout << x << ' ' << y << '\n';
+      reverse = false;
+      break;
     }
     x = y;
   }
 
-  std::cout << 'T' << std::endl;
+  infile.clear();
+  infile.seekg(0);
+  infile >> x;
+
+  while (infile >> y) {
+    if (x > y) {
+      order = false;
+      break;
+    }
+  }
+
+  if (reverse)
+    std::cout << "reverse" << '\n';
+  else if (order)
+    std::cout << "order" << '\n';
+  else
+    std::cout << "F" << '\n';
+  infile.close();
   return 0;
 }
