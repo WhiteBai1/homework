764b04c44fd66c9b45a6190d78abd4d195302a0a
WhiteBai1 <WhiteBai1@outlook.com>
2025-08-18 21:14:56 +0800

    Sort random, sequential, and reverse-ordered arrays using different algorithms

---
+++ timescale.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+#plot 1:
+xpoints = np.array([0,10,100,1000,10000,50000,100000,500000,1000000])
+randommerge = np.array([0,0.004,0.005,0.005,0.010,0.031,0.072,0.551,0.812])
+randomheap = np.array([0,0.005,0.005,0.005,0.010,0.032,0.157,0.374,1.017])
+randomquick = np.array([0,0.005,0.005,0.005,0.010,0.055,0.138,0.504,1.314])
+plt.subplot(1,3,1)
+plt.title("random")
+plt.plot(xpoints,randommerge,'-r',xpoints,randomheap,'-b',xpoints,randomquick,'-y')
+
+#plot2
+xpoints1 = np.array([0,10,100,1000,10000,50000,100000,500000])
+ordermerge = np.array([0,0.005,0.004,0.005,0.009,0.049,0.115,0.424,0.758])
+orderheap = np.array([0,0.005,0.004,0.005,0.009,0.043,0.101,0.437,0.943])
+orderquick = np.array([0,0.005,0.004,0.006,0.031,0.347,1.21,24.5])
+plt.subplot(1,3,2)
+plt.title("order")
+plt.plot(xpoints,ordermerge,':r',xpoints,orderheap,':b',xpoints1,orderquick,':y')
+
+#plot3
+xpoints2 = np.array([0,10,100,1000,10000,50000,100000])
+revmerge = np.array([0,0.004,0.005,0.005,0.008,0.056,0.094,0.431,0.842])
+revheap = np.array([0,0.005,0.004,0.005,0.009,0.065,0.082,0.449,0.816])
+revquick = np.array([0,0.004,0.005,0.006,0.055,1.042,3.908])
+plt.subplot(1,3,3)
+plt.title("reverse")
+plt.plot(xpoints,revmerge,'--r',xpoints,revheap,'--b',xpoints2,revquick,'--y')
+
+plt.xlabel("N")
+plt.ylabel("Time")
+
+plt.show()
